9d973c32356f3842ee6c639f9cc5a89011101ada
Maxime Daniel <root@maxux.net>
2018-05-04 03:54:59 +0200

    First working release

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# jumpscale-wheelhouse
+# Jumpscale Wheelhouse
+Build wheels (precompiled dependencies) for Jumpscale 9 (development branch)
+
+# Build
+- Clone this repository
+- Download image: `docker pull quay.io/pypa/manylinux1_x86_64`
+- Build target:
+  - `docker run -it --rm -v $(pwd)/core9:/io quay.io/pypa/manylinux1_x86_64 bash /io/build.sh`
+  - `docker run -it --rm -v $(pwd)/lib9:/io quay.io/pypa/manylinux1_x86_64 bash /io/build.sh`
+
+For each submodules (core9, lib9, ...) you'll find:
+ - Wheels under: `.../wheelhouse/repository` and `.../wheelhouse/repository`
+ - Archives for specific python version under: `.../wheelhouse/release/`
---
+++ lib9/build.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+set -ex
+
+export branch="development"
+export openssl="openssl-1.0.2o"
+export target="lib9"
+
+pushd /tmp
+
+wget ftp://ftp.openssl.org/source/${openssl}.tar.gz
+tar -xzf ${openssl}.tar.gz
+
+pushd ${openssl}
+./config --prefix=/usr --openssldir=/usr/openssl threads shared
+make -j 5
+make install_sw
+popd
+
+# cleaning existing wheelhouse
+rm -rf /io/wheelhouse/repository/*
+
+for pybin in $(ls -1d /opt/python/*{35,36}*/bin | grep -v cpython); do
+    "${pybin}/pip" install --upgrade cython
+
+    export CXXFLAGS="-DKJ_USE_EPOLL=0 -D__BIONIC__=1"
+
+    # preinstall pycapnp with specific build options
+    "${pybin}/pip" install pycapnp
+    "${pybin}/pip" wheel pycapnp -w /io/wheelhouse/repository/
+
+    unset CXXFLAGS
+done
+
+git clone -b ${branch} https://github.com/jumpscale/${target}
+pushd ${target}
+
+# extract install_require from setup.py
+sed -n '/install_require/,/\],/p;/\],/q' setup.py > requirements.txt
+
+# remove first and last lines
+sed -i '1,1d' requirements.txt
+sed -i '$ d' requirements.txt
+
+# clean python syntax
+sed -i "s/ //g;s/'//g;s/,//g" requirements.txt
+
+# temp fix
+sed -i '/^JumpScale/d' requirements.txt
+
+for pybin in $(ls -1d /opt/python/*{35,36}*/bin | grep -v cpython); do
+    "${pybin}/pip" install -r requirements.txt
+    "${pybin}/pip" freeze > /tmp/full-requirements.txt
+    "${pybin}/pip" wheel -r /tmp/full-requirements.txt -w /io/wheelhouse/repository/
+done
+
+for whl in /io/wheelhouse/repository/*.whl; do
+    if [[ $whl = *"none-any"* ]]; then continue; fi
+
+    auditwheel repair "$whl" -w /io/wheelhouse/repository/
+done
+
+popd
+popd
+
+
+# release archives
+pushd /io/wheelhouse/repository
+
+rm -rf /io/wheelhouse/release
+
+for version in cp35 cp36; do
+    endpoint="/io/wheelhouse/release/${version}"
+    mkdir -p ${endpoint}
+    rm -rf ${endpoint}/*
+
+    cp -av *none-any* ${endpoint}/
+    cp -av *${version}* ${endpoint}/
+
+    tar -czf /io/wheelhouse/release/jumpscale-${target}-${version}.tar.gz -C ${endpoint} .
+done
+
+ls -alh /io/wheelhouse/release/
+popd
